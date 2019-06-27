611c05fffd6cddf43f82bc40e9b4cb2b0ecdbefa
iyuershov <vaner1104@gmail.com>
2019-06-28 01:50:02 +0300

    Добавил вывод вёрстки сайта

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
-import  urllib.response
-from bs4 import BeautifulSoup
+import  urllib.request
+# from bs4 import BeautifulSoup
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
 
 def main():
-    pass
+    print(get_html('https://fedresurs.ru'))
+
+if __name__ == '__main__':
+    main()
