30a16e847bfa5f33c343d33126d24f8f3beee5e0
iyuershov <vaner1104@gmail.com>
2019-07-06 21:24:12 +0300

    Добавил получение списка огранизаций по названию

--- app/main.py
+++ app/main.py
@@ -1,15 +1,30 @@
-from urllib import request, parse
-# from bs4 import BeautifulSoup
+import requests
 
-def get_html(url):
-    response = request.urlopen(url)
-    return response.read()
+def get_entity_list(organization):
+    response = requests.post('https://fedresurs.ru/backend/companies/search', json={
+	"entitySearchFilter": {
+		"regionNumber": None,
+		"onlyActive": False,
+		"startRowIndex": 0,
+		"pageSize": 10000,
+		"code":organization['code'],
+		"name": organization['name'],
+		"legalCase": None
+	}
+})
+    return response.json()
 
 def main():
-    print("Введите название организации:")
-    entity = input()
-    query = "https://fedresurs.ru/search/entity?name=" + parse.quote(entity)
-    print(get_html(query))
+    print("Введите название или идентификатор организации организации:")
+    organization = dict.fromkeys(['code','name'])
+    entered = input()
+    if entered.isdigit():
+        organization.update({'code' : entered})
+    else:
+        organization.update({'name' : entered})
+    print(organization)
+
+    print(get_entity_list(organization))
 
 if __name__ == '__main__':
     main()
