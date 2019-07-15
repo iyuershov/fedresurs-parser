c64cb8fbbb5411a74ce63c8bb12598a82b845f23
iyuershov <vaner1104@gmail.com>
2019-07-15 18:32:12 +0300

    Добавил Flask и метод create_task

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 import requests
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
 
+#Получение списка организаций по ключевой фразе (название, инн или огрн)
 def get_entity_list(organization):
     response = requests.post('https://fedresurs.ru/backend/companies/search', json={
         "entitySearchFilter": {
             "regionNumber": None,
             "onlyActive": False,
             "startRowIndex": 0,
-            "pageSize": 2,
+            "pageSize": 10000000,
             "code": organization['code'],
             "name": organization['name'],
             "legalCase": None
@@ -18,10 +21,11 @@ def get_entity_list(organization):
     return response.json()
 
 
-def get_messages_info(guid):
+#Получение списка сообщений о банкротстве ("searchFirmBankruptMessage": True), относящихся к организации
+def get_messages_info(entity_guid):
     response = requests.post("https://fedresurs.ru/backend/companies/publications", json={
-        "guid": guid,
-        "pageSize": 1000000,
+        "guid": entity_guid,
+        "pageSize": 10000000,
         "startRowIndex": 0,
         "startDate": None,
         "endDate": None,
@@ -41,25 +45,25 @@ def get_messages_info(guid):
     return response.json()['pageData']
 
 
+#Создание конфига для работы headless-firefox
 def build_options():
     options = Options()
     options.headless = True
     return options
 
-
+@app.route('/create_task', methods=['GET'])
 def main():
-    print("Введите название или идентификатор организации организации:")
-    organization = dict.fromkeys(['code', 'name'])
-    entered = input()
-    if entered.replace(' ', '').isdigit():
-        organization.update({'code': entered.replace(' ', '')})
-    else:
-        organization.update({'name': entered})
+
+    #Параметры задаются в адресной строке
+    code = request.args.get('code')
+    name = request.args.get('name')
+
+    organization = dict(code=code,name=name)
 
     entity_list = get_entity_list(organization)
     if len(entity_list['pageData']) > 0:
         driver = webdriver.Firefox(options=build_options(), executable_path="driver/geckodriver")
-        result = []
+        result = dict()
         for entity in entity_list['pageData']:
             messages = get_messages_info(entity['guid'])
             message_list = []
@@ -75,16 +79,13 @@ def main():
                 message_list.append(data)
 
             if len(message_list) > 0:
-                result.append({entity['guid']: message_list})
-            else:
-                result.append({entity['guid']: "No bankrupt-themed messages."})
+                result[entity['guid']] = message_list
 
-        print(result)
+        return jsonify(result)
 
     else:
-        print("No such organizations")
-        exit(1)
+        return "No such organizations"
 
 
 if __name__ == '__main__':
-    main()
+    app.run()
