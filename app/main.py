b28204bf5abf7f2372351b5e0da2d0eaaab8ae20
iyuershov <vaner1104@gmail.com>
2019-07-18 21:07:12 +0300

    Задание 2 - выполнено

--- app/main.py
+++ app/main.py
@@ -1,90 +1,43 @@
-import requests
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from flask import Flask, request, jsonify
+import redis
+
+from rq import Queue
+from rq.job import Job
+from flask import Flask, request
+from app.service import get_messages
 
 app = Flask(__name__)
 
-#Получение списка организаций по ключевой фразе (название, инн или огрн)
-def get_entity_list(organization):
-    response = requests.post('https://fedresurs.ru/backend/companies/search', json={
-        "entitySearchFilter": {
-            "regionNumber": None,
-            "onlyActive": False,
-            "startRowIndex": 0,
-            "pageSize": 10000000,
-            "code": organization['code'],
-            "name": organization['name'],
-            "legalCase": None
-        }
-    })
-    return response.json()
+connection = redis.Redis()
+queue = Queue(connection=connection)
 
-
-#Получение списка сообщений о банкротстве ("searchFirmBankruptMessage": True), относящихся к организации
-def get_messages_info(entity_guid):
-    response = requests.post("https://fedresurs.ru/backend/companies/publications", json={
-        "guid": entity_guid,
-        "pageSize": 10000000,
-        "startRowIndex": 0,
-        "startDate": None,
-        "endDate": None,
-        "messageNumber": None,
-        "bankruptMessageType": None,
-        "bankruptMessageTypeGroupId": None,
-        "legalCaseId": None,
-        "searchAmReport": False,
-        "searchFirmBankruptMessage": True,
-        "searchFirmBankruptMessageWithoutLegalCase": False,
-        "searchSfactsMessage": False,
-        "searchSroAmMessage": False,
-        "searchTradeOrgMessage": False,
-        "sfactMessageType": None,
-        "sfactsMessageTypeGroupId": None
-    })
-    return response.json()['pageData']
-
-
-#Создание конфига для работы headless-firefox
-def build_options():
-    options = Options()
-    options.headless = True
-    return options
 
 @app.route('/create_task', methods=['GET'])
-def main():
-
-    #Параметры задаются в адресной строке
+def create_task():
+    # Параметры задаются в адресной строке
     code = request.args.get('code')
     name = request.args.get('name')
 
-    organization = dict(code=code,name=name)
+    if (code is not None) or (name is not None):
+        organization = dict(code=code, name=name)
 
-    entity_list = get_entity_list(organization)
-    if len(entity_list['pageData']) > 0:
-        driver = webdriver.Firefox(options=build_options(), executable_path="driver/geckodriver")
-        result = dict()
-        for entity in entity_list['pageData']:
-            messages = get_messages_info(entity['guid'])
-            message_list = []
-            for message in messages:
-                url = "https://bankrot.fedresurs.ru/MessageWindow.aspx?ID=" + message['guid']
-                driver.get(url)
-                data = {
-                    "guid": message['guid'],
-                    "text": driver.find_element_by_class_name("msg").text[7:],
-                    "date": message['datePublish'],
-                    "url": url
-                }
-                message_list.append(data)
+        with app.app_context():
+            task = queue.enqueue(get_messages, organization)
 
-            if len(message_list) > 0:
-                result[entity['guid']] = message_list
+        return f"Task {task.id} created at {task.enqueued_at}"
 
-        return jsonify(result)
+    return "Wrong parameters"
 
+
+@app.route('/get_task/<id>', methods=['GET'])
+def get_task(id):
+    if Job.exists(id,connection=connection):
+        job = Job.fetch(id, connection=connection)
+        if job.get_status() == "finished":
+            return job.result
+        else:
+            return job.get_status()
     else:
-        return "No such organizations"
+        return "No such task."
 
 
 if __name__ == '__main__':
---
+++ app/service.py
@@ -0,0 +1,88 @@
+import requests
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+# Получение списка организаций по ключевой фразе (название, инн или огрн)
+def get_entity_list(organization):
+    response = requests.post('https://fedresurs.ru/backend/companies/search', json={
+        "entitySearchFilter": {
+            "regionNumber": None,
+            "onlyActive": False,
+            "startRowIndex": 0,
+            "pageSize": 3,
+            "code": organization['code'],
+            "name": organization['name'],
+            "legalCase": None
+        }
+    })
+    return response.json()
+
+
+# Получение списка сообщений о банкротстве ("searchFirmBankruptMessage": True), относящихся к организации
+def get_messages_info(entity_guid):
+    response = requests.post("https://fedresurs.ru/backend/companies/publications", json={
+        "guid": entity_guid,
+        "pageSize": 3,
+        "startRowIndex": 0,
+        "startDate": None,
+        "endDate": None,
+        "messageNumber": None,
+        "bankruptMessageType": None,
+        "bankruptMessageTypeGroupId": None,
+        "legalCaseId": None,
+        "searchAmReport": False,
+        "searchFirmBankruptMessage": True,
+        "searchFirmBankruptMessageWithoutLegalCase": False,
+        "searchSfactsMessage": False,
+        "searchSroAmMessage": False,
+        "searchTradeOrgMessage": False,
+        "sfactMessageType": None,
+        "sfactsMessageTypeGroupId": None
+    })
+    return response.json()['pageData']
+
+
+# Создание конфига для работы headless-firefox
+def build_options():
+    options = Options()
+    options.headless = True
+    return options
+
+
+def get_messages(organization):
+    # Получаем список компаний по заданным параметрам
+    entity_list = get_entity_list(organization)
+
+    if len(entity_list['pageData']) > 0:
+
+        # Создаем экземпляра headless-firefox для получения текста сообщения
+        driver = webdriver.Firefox(options=build_options(), executable_path="driver/geckodriver")
+        result = dict()
+
+        for entity in entity_list['pageData']:
+
+            # Получаем список сообщений для каждой компании
+            messages = get_messages_info(entity['guid'])
+            message_list = []
+            for message in messages:
+                url = "https://bankrot.fedresurs.ru/MessageWindow.aspx?ID=" + message['guid']
+                driver.get(url)
+                text = driver.find_element_by_class_name('msg').text
+
+                data = {
+                    "id": message['guid'],
+                    "text": text,
+                    "date": message['datePublish'],
+                    "url": url
+                }
+                message_list.append(data)
+
+            if len(message_list) > 0:
+                result[entity['guid']] = message_list
+
+        return result
+
+    else:
+        return "No such organizations"
